0b8d8533fcdbea1fe17432e453871bea73117846
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-01-07 10:49:05 +0300

    feat: practise

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// import retryTimes = jest.retryTimes;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Rating/>
+            <AppTitle/>
+            <Rating/>
+        </div>
+    );
+}
+
+const Star = () => {
+    return (
+        <div>
+            Star
+        </div>
+    )
+}
+
+const Rating = () => {
+    return (
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+    )
+}
+
+const AppTitle = () => {
+    return (<div className={"h1"}>
+            <h1>Happy hacking!</h1>
+        </div>
+    )
 }
 
 export default App;
