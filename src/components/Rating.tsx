246f82f94e19704735e39ee3f3654e31ca284829
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-01-13 18:41:42 +0300

    refactor: avoid code duplication

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -7,68 +7,13 @@ type RatingPropsType = {
 
 
 export const Rating = (props: RatingPropsType) => {
-    if (props.value === 1) {
-        return (
-            <div>
-                <Star bolded={true}/>
-                <Star bolded={false}/>
-                <Star bolded={false}/>
-                <Star bolded={false}/>
-                <Star bolded={false}/>
-            </div>
-        )
-    }
-    if (props.value === 2) {
-        return (
-            <div>
-                <Star bolded={true}/>
-                <Star bolded={true}/>
-                <Star bolded={false}/>
-                <Star bolded={false}/>
-                <Star bolded={false}/>
-            </div>
-        )
-    }
-    if (props.value === 3) {
-        return (
-            <div>
-                <Star bolded={true}/>
-                <Star bolded={true}/>
-                <Star bolded={true}/>
-                <Star bolded={false}/>
-                <Star bolded={false}/>
-            </div>
-        )
-    }
-    if (props.value === 4) {
-        return (
-            <div>
-                <Star bolded={true}/>
-                <Star bolded={true}/>
-                <Star bolded={true}/>
-                <Star bolded={true}/>
-                <Star bolded={false}/>
-            </div>
-        )
-    }
-    if (props.value === 5) {
-        return (
-            <div>
-                <Star bolded={true}/>
-                <Star bolded={true}/>
-                <Star bolded={true}/>
-                <Star bolded={true}/>
-                <Star bolded={true}/>
-            </div>
-        )
-    }
     return (
         <div>
-            <Star bolded={false}/>
-            <Star bolded={false}/>
-            <Star bolded={false}/>
-            <Star bolded={false}/>
-            <Star bolded={false}/>
+            <Star bolded={props.value > 0}/>
+            <Star bolded={props.value > 1}/>
+            <Star bolded={props.value > 2}/>
+            <Star bolded={props.value > 3}/>
+            <Star bolded={props.value > 4}/>
         </div>
     )
 }
