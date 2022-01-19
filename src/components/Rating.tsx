32b965e6f4aaefe1c662ea76f784d33319bd3ad4
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-01-19 09:10:49 +0300

    practise

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -7,17 +7,18 @@ export const Rating = (props: RatingProps) => {
     console.log("Rating rendering");
 
     const [value, setValue] = useState(0)
-
-
     return (
         <div>
-            <Star bolded={value > 0}/><button onClick={()=>setValue(1)}>1</button>
-            <Star bolded={value > 1}/><button onClick={()=>setValue(2)}>2</button>
-            <Star bolded={value > 2}/><button onClick={()=>setValue(3)}>3</button>
-            <Star bolded={value > 3}/><button onClick={()=>setValue(4)}>4</button>
-            <Star bolded={value > 4}/><button onClick={()=>setValue(5)}>5</button>
+            <Star bolded={value > 0}/>
+            <button onClick={() => setValue(1)}>1</button>
+            <Star bolded={value > 1}/>
+            <button onClick={() => setValue(2)}>2</button>
+            <Star bolded={value > 2}/>
+            <button onClick={() => setValue(3)}>3</button>
+            <Star bolded={value > 3}/>
+            <button onClick={() => setValue(4)}>4</button>
+            <Star bolded={value > 4}/>
+            <button onClick={() => setValue(5)}>5</button>
         </div>
     )
-
-
 }
